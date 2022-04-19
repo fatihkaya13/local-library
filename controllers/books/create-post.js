adfc6f445612fc5af6958185adda05c3286c99d8
Fatih Kaya <56218812+fatihkaya13@users.noreply.github.com>
2022-04-19 15:02:26 +0300

    call function update book and insert a dummy value to dummy database for test

--- controllers/books/create-post.js
+++ controllers/books/create-post.js
@@ -1,4 +1,7 @@
 const Book = require("../../models/Book");
+const {
+  createClickhouseClient,
+} = require("../../loaders/clickhouse.js");
 
 module.exports = function (req, res, next) {
   const book = new Book({
@@ -8,6 +11,21 @@ module.exports = function (req, res, next) {
     isbn: req.body.isbn,
     genre: req.body.genre,
   });
+
+
+  const ch = createClickhouseClient();
+
+  // test connection and insert to another db
+  ch.query(
+    `INSERT INTO 
+      VALUES ('customer6', '2021-06-10', 'add_to_cart', 'US', 66666 )`,
+    (err, data) => {
+      if (err) {
+        console.log(err);
+        return next(err);
+      } else console.log("values are inserted");
+    }
+  );
 
   book.save(function (err) {
     if (err) return next(err);
