66af4cfe358a8c2d9d0c60899e98666e11d25b24
Fatih Kaya <56218812+fatihkaya13@users.noreply.github.com>
2022-04-20 14:07:16 +0300

    book and author join query for clickhouse then display results

--- controllers/books/list.js
+++ controllers/books/list.js
@@ -1,13 +1,51 @@
-const Book = require('../../models/Book');
+const Book = require("../../models/Book");
+
+const { createClickhouseClient } = require("../../loaders/clickhouse.js");
 
 module.exports = function (req, res, next) {
-  Book
-    .find({}, 'title author')
+
+  // create instance of click house 
+  const ch = createClickhouseClient();
+  const query = `select b.title, a.id as _id, a.first_name, a.family_name, a.date_of_birth, a.date_of_death
+                  from locallibrary.book as b
+                  join locallibrary.author as a
+                  on locallibrary.author.id = locallibrary.book.author_id
+                  order by b.title`;
+  // execute the query
+  ch.querying(query, (err, result) => {
+    if (err) return next(err);
+  }).then((queryResponse) => {
+    let bookQueryArrObj = queryResponse.data;
+
+    bookQueryArrObj = queryResponse.data.map(
+      ([title, _id, first_name, family_name, date_of_birth, date_of_death]) => {
+        return {
+          title,
+          author: {
+            _id,
+            first_name,
+            family_name,
+            date_of_birth,
+            date_of_death,
+          },
+        };
+      }
+    );
+    console.log(bookQueryArrObj);
+
+    res.render("book_list", { title: "Book List", book_list: bookQueryArrObj });
+  });
+
+  /*
+  Book.find({}, "title author")
     .sort({ title: 1 })
-    .populate('author')
+    .populate("author")
     .exec(function (err, listBooks) {
       if (err) return next(err);
 
-      res.render('book_list', { title: 'Book List', book_list: listBooks });
+      console.log(listBooks);
+
+      res.render("book_list", { title: "Book List", book_list: listBooks });
     });
+    */
 };
