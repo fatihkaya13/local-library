a4136a866b6107f854f37b1ee799d5b4bda9563a
Fatih Kaya <56218812+fatihkaya13@users.noreply.github.com>
2022-04-20 14:05:54 +0300

    run parallel genre and author query for clickhouse then display results

--- controllers/books/create-get.js
+++ controllers/books/create-get.js
@@ -1,19 +1,85 @@
-const { parallel } = require('async');
+const { parallel } = require("async");
 
-const Author = require('../../models/Author');
-const Genre = require('../../models/Genre');
+const { createClickhouseClient } = require("../../loaders/clickhouse.js");
+
+// const Author = require("../../models/Author");
+// const Genre = require("../../models/Genre");
 
 module.exports = function (req, res, next) {
-  parallel({
-    authors: function (callback) {
-      Author.find(callback);
-    },
-    genres: function (callback) {
-      Genre.find(callback);
-    }
-  }, function (err, results) {
-    if (err) return next(err);
+  const ch = createClickhouseClient();
 
-    res.render('book_form', { title: 'Create Book', authors: results.authors, genres: results.genres });
-  });
+  const authorQuery = `select * from locallibrary.author`;
+  const genresQuery = `select * from locallibrary.genre`;
+
+  let results = { authors: [], genres: [] };
+
+  parallel(
+    ch
+      .querying(genresQuery, (err, result) => {
+        if (err) return next(err);
+      })
+      .then((res) => {
+        let genresQueryArrObj = res.data;
+        genresQueryArrObj = res.data.map(([_id, name]) => {
+          return {
+            _id,
+            name,
+          };
+        });
+        results.genres = genresQueryArrObj;
+      }),
+    ch
+      .querying(authorQuery, (err, result) => {
+        if (err) return next(err);
+      })
+      .then((queryResponse) => {
+        let authorQueryArrObj = queryResponse.data;
+
+        // convert nested array to array of objects
+        authorQueryArrObj = queryResponse.data.map(
+          ([_id, first_name, family_name, date_of_birth, date_of_death]) => {
+            return {
+              _id,
+              first_name,
+              family_name,
+              date_of_birth,
+              date_of_death,
+            };
+          }
+        );
+        results.authors = authorQueryArrObj;
+
+        console.log(results);
+
+        res.render("book_form", {
+          title: "Create Book",
+          authors: results.authors,
+          genres: results.genres,
+        });
+      })
+  );
+
+  /*
+  parallel(
+    {
+      authors: function (callback) {
+        Author.find(callback);
+      },
+      genres: function (callback) {
+        Genre.find(callback);
+      },
+    },
+    function (err, results) {
+      if (err) return next(err);
+
+      
+      res.render("book_form", {
+        title: "Create Book",
+        authors: results.authors,
+        genres: results.genres,
+      });
+
+    }
+  );
+  */
 };
