652df8494c6e9ff42b9de18436b18a098eb6db06
Fatih Kaya <56218812+fatihkaya13@users.noreply.github.com>
2022-04-21 23:26:49 +0300

    Update app.js

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ const bookInstanceRouter = require("./routes/book-instance");
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
-app.use(logger("dev"));
+// app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
